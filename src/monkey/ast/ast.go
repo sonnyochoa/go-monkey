1694ffb5de36ad2574483042ebfc730863618982
Sonny Ochoa <0xsonny@gmail.com>
2019-04-17 05:10:05 -0700

    Define AST construction.

---
+++ src/monkey/ast/ast.go
@@ -0,0 +1,56 @@
+/*
+  let x = 10;
+  let y = 15;
+
+  let add = fn(a, b) {
+    return a + b;
+  };
+
+  let <identifier> = <expression>;
+*/
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.TokenLiteral }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.TokenLiteral }
